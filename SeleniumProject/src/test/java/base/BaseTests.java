0293f02b1b784583c54a958b8197d927de72ca7a
Radost Koleva <radostkoleva0142@gmail.com>
2024-01-02 15:19:56 +0200

    Initial commit.

---
+++ SeleniumProject/src/test/java/base/BaseTests.java
@@ -0,0 +1,31 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+    private WebDriver driver;
+
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        //driver.manage().window().setSize(new Dimension(375, 812));
+        driver.manage().window().maximize();
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
