cee552c079921ee44b8190684fce5aa65a5d37d0
Radost Koleva <radostkoleva0142@gmail.com>
2024-07-05 11:32:44 +0300

    Small fixes.

--- AuthenticationSeleniumProject/src/main/java/models/TwoFA.java
+++ AuthenticationSeleniumProject/src/main/java/models/TwoFA.java
@@ -1,23 +1,10 @@
 package models;
 
+import lombok.Data;
+
+@Data
 public class TwoFA {
 
     private String secret;
     private boolean enabled;
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
--- AuthenticationSeleniumProject/src/test/java/RememberMeTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class RememberMeTests extends BaseTests {
-
-    @Test
-    public void loginSuccessfully_usingEmail() {
-
-    }
-}
